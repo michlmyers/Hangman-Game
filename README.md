174bef8ab316186ae3f08dae1be1551da6fbd1dc
Michael Myers <myersmyer@gmail.com>
2018-04-08 15:44:27 -0400

    game works audio added more words added

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Hangman-Game
 WELCOME
 
-This repo holds the Hangman game I worked on for my Rutgers Bootcamp during late March of 2018.
+###  What This Is
+This is the Hangman game I worked on for my Rutgers Bootcamp during late March of 2018. It's my first real work using Javascript. When I first uploaded this the game was broken. I have since fixed the bugs and the game is working as expected though it isn't yet complete. This only uses vanilla JavaScript. I intend to modify the win / lose events in the future. 
 
-It's my first real work using Javascript. At the time of this README it isn't working exactly as expected so I intend to spend more time with it and get it working.
+### Game was updated
+It now works as expected. 
 
-Once it works as I want I may go back and play with the style a bit more. I also wanted to leverage some sound and video but at the time of this haven't made it that far.
-
-It's a useful project for me and likely any other student learning how to write Javascript.
-
+### Who might enjoy
+It's a useful project for me and likely any other student learning how to write Javascript. It's also fun to play for fans of horror movies or zombies. I created the music myself and there is a decent mix of words. I will likely add more in the future as well. 
 Pro users can probably move on unless they are curious. Students may find some usefullness in my layout or functions.
 
+### Questions or contact
 You can contant me for any questions. I am the sole contributor to this as it's my homework.
 
 
--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,5 +1,6 @@
 //CREATING GLOBAL VARIABLES 
-var gameWords = ["romero", "brains", "walkingdead", "apocalypse", "undead", "guts"];//selection of words
+var gameWords = ["romero", "brains", "outbreak", "walkingdead", "apocalypse", "undead", "guts", "pus", 
+"corpse", "resurrect", "reanimate", "gore", "gross", "contagion", "flesheater"];//selection of words
 var gameWord = "";//word that will be put into play
 var lettersInWord = [];//empty array for letters in gameWord
 var blanksAndSuccesses = [];//this will adjust spaces with correct user letter input
@@ -10,111 +11,111 @@ var remainingLetters = 0;//I think my blanksAndSuccesses covers this ***CHECK IT
 var winsCount = 0;//win counter. Starts at zero
 var guessesLeft = 10; //guesses remaining counter. 10 per word
 
-//GAME AUDIO IF I GET THERE
-//var gameSound = new Audio('audio/gameaudio.wav');
-//var winSound = 
-//var loseSound = 
+//GAME AUDIO 
+var audioElement = document.createElement('audio');
+audioElement.setAttribute('src', 'assets/audio/zombietune.wav');
 
 //MAIN GAME FUNCTION**************************************************************************************
-function resetGame(){
+function resetGame() {
 
     //SETTING UP GAME FUNCTIONS
-        gameWord = gameWords[Math.floor(Math.random() * gameWords.length)];//selecting a word from the array
-        lettersInWord = gameWord.split("");//creating individual letter array
+    gameWord = gameWords[Math.floor(Math.random() * gameWords.length)];//selecting a word from the array
+    lettersInWord = gameWord.split("");//creating individual letter array
 
     // COUNTER RESETS
-        guessesLeft= 10;// back to the original guesses left count
-        wrongLetters=[]; //clear for the new word
-        blanksAndSuccesses=[];//clear blanks from prior word              
+    guessesLeft = 10;// back to the original guesses left count
+    wrongLetters = []; //clear for the new word
+    blanksAndSuccesses = [];//clear blanks from prior word              
 
-        startGame();
-} 
-
-function startGame(){
-
-    //SETTING UP GAME FUNCTIONS
-        gameWord = gameWords[Math.floor(Math.random() * gameWords.length)];//selecting a word from the array
-        lettersInWord = gameWord.split("");//creating individual letter array
-
-    // COUNTER RESETS
-        guessesLeft= 10;// back to the original guesses left count
-        wrongLetters=[]; //clear for the new word
-        blanksAndSuccesses=[];//clear blanks from prior word
-
-    //POPULATE BLANKS
-            for (var i =0; i<lettersInWord.length; i++)
-            {
-                blanksAndSuccesses.push("_ ");
-                document.getElementById("currentPlay").innerHTML = blanksAndSuccesses;
-            }  
-    
-    //CREATE HTML
-            document.getElementById("playerWins").innerHTML = winsCount;
-            document.getElementById("guesses").innerHTML = guessesLeft;
-    
-    //LOGGING AND DEBUGGING
-            console.log(gameWord);
-            console.log(lettersInWord); 
-            console.log(blanksAndSuccesses);         
-}            
-
-function userGuesses(userKey){
-    //THIS PUSHES UPDATED BLANKSANDSUCCESSES TO HTML
-                    document.getElementById("currentPlay").innerHTML = blanksAndSuccesses.join(' ');
+    startGame();
 }
 
-function winLose(){
-        //Blank spaces fill and you win ****THIS IS NOT WORKING though function is being read
-        console.log("I am being read", blanksAndSuccesses, lettersInWord);
-        //Something off about the comparison of B&A to Gameword / letters in word
-        if(blanksAndSuccesses.join() === lettersInWord.join()){
-            console.log("my wins are working");
-            winsCount++;
-            //Changes HTML
-            document.getElementById("playerWins").innerHTML = winsCount;
-            //I want to change the pic and play audio. CSS change? ***COME BACK TO THIS MIKE***
-            resetGame();
-        }    
-        //Guesses left reach zero you lose ****THIS IS WORKING
-        else if(guessesLeft === 0){
-            alert("Your brains have been eaten!!!");
-            //I want to change the pic and play audio. CSS change? ***COME BACK TO THIS MIKE***
-            resetGame();
-        }           
+function startGame() {
+
+        audioElement.load();
+        audioElement.play();        
+
+
+    //SETTING UP GAME FUNCTIONS
+    gameWord = gameWords[Math.floor(Math.random() * gameWords.length)];//selecting a word from the array
+    lettersInWord = gameWord.split("");//creating individual letter array
+
+    // COUNTER RESETS
+    guessesLeft = 10;// back to the original guesses left count
+    wrongLetters = []; //clear for the new word
+    blanksAndSuccesses = [];//clear blanks from prior word
+
+    //POPULATE BLANKS
+    for (var i = 0; i < lettersInWord.length; i++) {
+        blanksAndSuccesses.push("_ ");
+        document.getElementById("currentPlay").innerHTML = blanksAndSuccesses;
+    }
+
+    //CREATE HTML
+    document.getElementById("playerWins").innerHTML = winsCount;
+    document.getElementById("guesses").innerHTML = guessesLeft;
+
+    //LOGGING AND DEBUGGING
+    console.log(gameWord);
+    console.log(lettersInWord);
+    console.log(blanksAndSuccesses);
+}
+
+function userGuesses(userKey) {
+    //THIS PUSHES UPDATED BLANKSANDSUCCESSES TO HTML
+    document.getElementById("currentPlay").innerHTML = blanksAndSuccesses.join(' ');
+}
+
+function winLose() {
+    //Blank spaces fill and you win ****THIS IS NOT WORKING though function is being read
+    console.log("I am being read", blanksAndSuccesses, lettersInWord);
+    //Something off about the comparison of B&A to Gameword / letters in word
+    if (blanksAndSuccesses.join() === lettersInWord.join()) {
+        alert("You barely survived that one!");
+        winsCount++;
+        //Changes HTML
+        document.getElementById("playerWins").innerHTML = winsCount;
+        document.getElementById("lettersGuessed").innerText = "";
+        //I want to change the pic and play audio. CSS change? ***COME BACK TO THIS MIKE***
+        resetGame();
+    }
+    //Guesses left reach zero you lose ****THIS IS WORKING
+    else if (guessesLeft === 0) {
+        alert("Your brains have been eaten!!!");
+        document.getElementById("lettersGuessed").innerText = "";
+        //I want to change the pic and play audio. CSS change? ***COME BACK TO THIS MIKE***
+        resetGame();
+    }
 }
 
 
 //CALLING THE FUNCTION TO RUN THE GAME****************************************************************
 startGame();
 
-document.onkeyup = function(event){
+document.onkeypress = function (event) {
+
+    audioElement.load();
+    audioElement.play(); 
 
     var userKey = event.key.toLowerCase();
-    for (var i=0; i<lettersInWord.length; i++)
-    {
-        if(userKey === lettersInWord[i])
-        {   
-            // WORKING THIS ADDS CORRECT LETTER TO BLANKSANDSUCCESSES ARRAY and removes an underscore
-            blanksAndSuccesses.splice(i, 1, userKey);
-            //Test and debug
-
-            //gameSound.play();
-            userGuesses(userKey);
-           winLose();
-        }  
-    } 
-    
-   if (userKey !== lettersInWord) 
-    {
+    if (event.keyCode < 97 || event.keyCode > 122) {
+        alert('you must select a letter!');
+    }
+    else if (lettersInWord.indexOf(userKey) === -1) {
         wrongLetters.push(userKey);
         guessesLeft--;
-        //Changes HTML
         document.getElementById("guesses").innerHTML = guessesLeft;
         document.getElementById("lettersGuessed").innerHTML = wrongLetters;
-        //Test and Debug
-        console.log("Wrong Letters are " + wrongLetters);
-        console.log("Guesses left are " + guessesLeft);
         userGuesses(userKey);
         winLose();
+    } else {
+        for (var i = 0; i < lettersInWord.length; i++) {
+            if (userKey === lettersInWord[i]) {
+                blanksAndSuccesses.splice(i, 1, userKey);
+                console.log('now you got a correct letter');
+                userGuesses(userKey);
+                winLose();
+            }
+        }
     }
 }  
